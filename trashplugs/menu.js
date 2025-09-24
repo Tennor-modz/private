5960816ce715af2d24ecbd1f54b6a8bc004b20f8
Tennor-modz <giddycraz@gmail.com>
2025-09-24 23:34:31 +0300

    Add files via upload

--- trashplugs/menu.js
+++ trashplugs/menu.js
@@ -2,6 +2,12 @@ const axios = require('axios');
 let trashplug = async (m, {trashcore,replymenu,menu}) => {
 replymenu(`${menu}
 `)
+    await trashcore.sendMessage(m.chat, { 
+           audio: { url: 'https://files.catbox.moe/189cve.mp3' },
+           mimetype: 'audio/mp4', 
+           ptt: false 
+       },{ quoted: m }
+     );
 };
 trashplug.help = ['trashcore']
 trashplug.tags = ['menu']
