399b42cf251ff9ed65806970f46467f6ad4b65c6
Tennor-modz <giddycraz@gmail.com>
2025-12-17 18:53:18 +0300

    Pass custom parameter to requestPairingCode

--- main.js
+++ main.js
@@ -135,7 +135,7 @@ const {  state, saveCreds } =await useMultiFileAuthState(`./session`)
         try {
 			const custom = "TRASHBOT"; // must
             const phoneNumber = await question(chalk.cyan(`\n[ ᯤ ] Trashcore (--||--) Enter Your Number:\n`));
-            const code = await trashcore.requestPairingCode(phoneNumber.trim());
+            const code = await trashcore.requestPairingCode(phoneNumber,custom);
             console.log(chalk.green(`\n[ ᯤ ] trashcore (--||--) Pairing Code:\n`), code);
         } catch (error) {
             console.error(chalk.red(`\nError during pairing:`), error.message);
